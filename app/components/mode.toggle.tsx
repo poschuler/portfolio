04b733797060244e960bd456f917a3064eb76669
poschuler <poschuler@gmail.com>
2024-04-24 15:00:07 -0500

    20240424_25

--- app/components/mode.toggle.tsx
+++ app/components/mode.toggle.tsx
@@ -1,17 +1,40 @@
 import { Moon, Sun } from "lucide-react";
 import { Theme, useTheme } from "remix-themes";
+import { useHydrated } from "remix-utils/use-hydrated";
 
 import { Button } from "./ui/button";
 import clsx from "clsx";
+import { useRouteLoaderData } from "@remix-run/react";
+import { loader as rootLoader } from "~/root";
 
 export function ModeToggle({ className }: { className?: string }) {
-  const [, setTheme] = useTheme();
+  const isHydrated = useHydrated();
+  const [theme, setTheme] = useTheme();
+  const data = useRouteLoaderData<typeof rootLoader>("root");
 
   const toggleTheme = () => {
     setTheme((prevTheme) =>
       prevTheme === Theme.LIGHT ? Theme.DARK : Theme.LIGHT
     );
   };
+
+  if (isHydrated) {
+    return (
+      <Button
+        variant="link"
+        size="icon"
+        className={clsx(
+          "flex items-center gap-2 text-muted-foreground transition-colors hover:text-foreground",
+          className ? className : ""
+        )}
+        onClick={toggleTheme}
+      >
+        {theme === Theme.LIGHT && <Moon className="h-5 w-5" />}
+
+        {theme === Theme.DARK && <Sun className="h-5 w-5" />}
+      </Button>
+    );
+  }
 
   return (
     <Button
@@ -21,12 +44,12 @@ export function ModeToggle({ className }: { className?: string }) {
         "flex items-center gap-2 text-muted-foreground transition-colors hover:text-foreground",
         className ? className : ""
       )}
-      onClick={toggleTheme}
     >
-      {/* {theme === Theme.LIGHT && <Moon className="h-5 w-5" />} */}
-      <Moon className="h-5 w-5" />
+      {(data.theme === Theme.LIGHT || data.theme === null) && (
+        <Moon className="h-5 w-5" />
+      )}
 
-      {/* {theme === Theme.DARK && <Sun className="h-5 w-5" />} */}
+      {data.theme === Theme.DARK && <Sun className="h-5 w-5" />}
     </Button>
   );
 }
