f154f5c1ac3717f8a59d52b6f6cb407eb9e545bb
poschuler <poschuler@gmail.com>
2024-04-23 23:11:13 -0500

    vercel

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,14 @@ import { vitePlugin as remix } from "@remix-run/dev";
 import { installGlobals } from "@remix-run/node";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { vercelPreset } from '@vercel/remix/vite';
 
 installGlobals();
 
 export default defineConfig({
-  plugins: [remix(), tsconfigPaths()],
+  plugins: [remix({
+    presets: [vercelPreset()],
+  }), tsconfigPaths()],
   server: {
     open: true,
   },
