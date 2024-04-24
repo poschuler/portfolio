52a260f49bdf90e532d0acd8fc4d3c6d0be3ef83
poschuler <poschuler@gmail.com>
2024-04-24 13:38:27 -0500

    20240424_14

--- app/components/mode.toggle.tsx
+++ app/components/mode.toggle.tsx
@@ -1,19 +1,14 @@
 import { Moon, Sun } from "lucide-react";
 import { Theme, useTheme } from "remix-themes";
-
 import { Button } from "./ui/button";
-import clsx from "clsx";
 
-export function ModeToggle({ className }: { className?: string }) {
+export function ModeToggle() {
   const [theme, setTheme] = useTheme();
   return (
     <Button
       variant="link"
       size="icon"
-      className={clsx(
-        "flex items-center gap-2 text-muted-foreground transition-colors hover:text-foreground",
-        className
-      )}
+      className="flex items-center gap-2 text-muted-foreground transition-colors hover:text-foreground"
       onClick={() => setTheme(theme === Theme.DARK ? Theme.LIGHT : Theme.DARK)}
     >
       {theme === Theme.LIGHT && <Moon className="h-5 w-5" />}
