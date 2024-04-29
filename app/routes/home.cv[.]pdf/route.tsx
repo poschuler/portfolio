a880e9493a463b4bce114a87b3b2c49c4f32684e
poschuler <poschuler@gmail.com>
2024-04-29 09:43:15 -0500

    cdn pdf

--- app/routes/home.cv[.]pdf/route.tsx
+++ app/routes/home.cv[.]pdf/route.tsx
@@ -1,14 +1,6 @@
-//import pdfFile from "./cv.pdf";
-import { promises as fs } from "fs";
-import path from "path";
-
 export async function loader() {
-  //const filePath = path.resolve("/");
-
-  //const pdfStream = await fs.readFileSync("/root/app/files/cv.pdf");
-  //console.log(pdfFile);
-  const filePath = path.join("public", "cv.pdf");
-  const data = await fs.readFile(filePath);
+  const response = await fetch("https://cdn.poschuler.dev/cv.pdf");
+  const data = await response.arrayBuffer();
 
   return new Response(data, {
     status: 200,
