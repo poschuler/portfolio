82ae1ead47456024583d16f255839b38e91a5bef
poschuler <poschuler@gmail.com>
2025-11-07 19:16:44 -0500

    disable middleware

--- react-router.config.ts
+++ react-router.config.ts
@@ -4,9 +4,9 @@ import type { Config } from "@react-router/dev/config";
 export default {
   // Config options...
   // Server-side render by default, to enable SPA mode set this to `false`
-  future: {
-    v8_middleware: true,
-  },
+  // future: {
+  //   v8_middleware: true,
+  // },
   ssr: true,
   presets: [vercelPreset()],
 } satisfies Config;
